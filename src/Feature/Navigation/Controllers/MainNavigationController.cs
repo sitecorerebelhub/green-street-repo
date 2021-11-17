c8b812f2ba4ee20a9dfec3145881fa5241a62bf7
chirayu <chirayu3589@gmail.com>
2021-11-17 08:53:58 +0100

    Custom Navigation

---
+++ src/Feature/Navigation/Controllers/MainNavigationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.XA.Foundation.Mvc.Controllers;
+
+namespace GreenStreet.Feature.Navigation.Controllers
+{
+    public class MainNavigationController : StandardController
+    {
+        // GET: MainNavigation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
